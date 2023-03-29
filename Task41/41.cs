7478eb7659ff6d186ac9c803404059191304fe12
Andrey Zhulay <zhulay.00@mail.ru>
2023-03-29 20:33:45 +0300

    first commit

---
+++ Task41/41.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 41:
+Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Write($"Ввод чисел: ");
+string [] numbers = Console.ReadLine()!.Split(' ', ',' , ';', '.');
+
+int [] array= new int[numbers.Length];
+
+for (int i = 0; i < array.Length; i++)
+    int.TryParse(numbers[i], out array[i]);
+    for (int i = 0; i < array.Length; i++)
+    Console.Write($" {array[i]} ");
+
+    int Elem = 0;
+
+for (int i = 0; i < array.Length; i++)
+
+    if (array[i] > 0)
+     
+    Elem++;
+
+    Console.Write($" Количество положительных чисел:  {Elem} ");
+
+    return Elem;
+     
+    
+
+
+
+
+
+
+
---
+++ Task43/43.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+int b1, k1, b2, k2;
+
+
+Console.Write($"Введите значение k1: ");
+int.TryParse(Console.ReadLine(),out  b1);
+
+Console.Write($"Введите значение k1: ");
+int.TryParse(Console.ReadLine(),out  k1);
+
+Console.Write($"Введите значение k2: ");
+int.TryParse(Console.ReadLine(),out  b2);
+
+Console.Write($"Введите значение b2: ");
+int.TryParse(Console.ReadLine(),out  k2);
+
+
+
+double x = (b2 - b1) / (k2 - k1);
+double y = k1 * x + b1;
+
+Console.Write($"координаты: {x}, {y}");
